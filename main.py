a029c0e55c8461d847b0f43cd21dc194e2f4969e
Victor Ponce <vicmanuelr@gmail.com>
2022-10-12 20:55:23 -0600

    Set send message to send the news on each new list
    - used sandbox twilio for whatssap testing
    - currently set to send always a message since difference for the stock
     is lower than 5%

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import requests
 import datetime as dt
 import os
+import requests
+from twilio.rest import Client
+
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+NEWS_KEY = os.environ.get("NEWS_API")
+NEWS_ENDPOINT = "https://newsapi.org/v2/top-headlines"
+NEWS_PARAMETERS = {
+    "apiKey": NEWS_KEY,
+    "q": "Tesla",
+}
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 API_KEY = os.environ.get("API_KEY")
 PARAMETERS_ALPH = {
@@ -15,9 +20,11 @@ PARAMETERS_ALPH = {
     "outputsize": "compact",
     "apikey": API_KEY
 }
+ACC_SID = os.environ.get("ACC_SID")
+TWILIO_KEY = os.environ.get("TWILIO_KEY")
 
 
-def get_stock_data():
+def get_stock_data() -> dict:
     response = requests.get(STOCK_ENDPOINT, params=PARAMETERS_ALPH)
     response.raise_for_status()
     data = response.json()
@@ -38,20 +45,45 @@ today_price = float(series_dict[str(date_objects[0])]["4. close"])
 last_closing_price = float(series_dict[str(date_objects[1])]["4. close"])
 
 # calculate percentage difference from 2 last closing prices
-difference = abs(today_price - last_closing_price)/(today_price/2 + last_closing_price/2) * 100
+difference = round(abs(today_price - last_closing_price) / (today_price / 2 + last_closing_price / 2) * 100, 2)
+if today_price > last_closing_price:
+    symbol = "🔺"
+else:
+    symbol = "🔻"
+
+
+def get_news() -> list:
+    response = requests.get(NEWS_ENDPOINT, params=NEWS_PARAMETERS)
+    response.raise_for_status()
+    data = response.json()
+    list_of_news = data["articles"][:3]
+    return list_of_news
+
+
+client = Client(username=ACC_SID, password=TWILIO_KEY)
+
+
+def send_message(client_object: Client, msg_title: str, msg_description: str):
+    message = client_object.messages.create(
+        body=f"{STOCK}:{symbol}{difference}%\nHeadline:{msg_title}\nBrief:{msg_description}",
+        from_='whatsapp:+14155238886',
+        to='whatsapp:+50230324739'
+    )
+    print(message)
+
 
 if difference > 5:
-    print("Get News")
+    for news in get_news():
+        title = news["title"]
+        description = news["description"]
+        send_message(client, title, description)
 else:
-    print("Difference less than 5")
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+    for news in get_news():
+        title = news["title"]
+        description = news["description"]
+        send_message(client, title, description)
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
-
-
-#Optional: Format the SMS message like this: 
+# Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
@@ -65,4 +97,3 @@ Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and 
 by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the 
 coronavirus market crash.
 """
-
